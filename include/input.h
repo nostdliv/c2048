546ef7113351b5f3c82c5cb2581c51e0b21746e1
nostdliv <koolkupbord@gmail.com>
2021-02-28 06:54:12 +0300

    Removed CStrLL dependency

--- include/input.h
+++ include/input.h
@@ -2,14 +2,8 @@
 #define INPUT_H
 
 #include <def.h>
-#include <CStringLinkedList.h>
 
 // returns 1 if redraw is needed, otherwise returns 0
 int handleInput(const char* input);
-
-typedef struct {
-    enum ACTION action;
-    CStrLL_t* ll_strings;
-} cmd_t;
 
 #endif
